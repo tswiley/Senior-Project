89360b9a92c9412f25befb413b3a4a887aed93c4
Travis Wiley <traviswiley88@gmail.com>
2023-11-09 16:10:05 -0800

    changes to header css, js fetch header footer

--- faq.js
+++ faq.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Fetch and include the common content
-    fetch("common.html")
+    // Fetch and include the header content
+    fetch("header.html")
         .then(response => response.text())
-        .then(data => {
-            document.body.innerHTML = data;
+        .then(headerData => {
+            // Fetch and include the footer content after fetching the header
+            fetch("footer.html")
+                .then(response => response.text())
+                .then(footerData => {
+                    // Manipulate the DOM to include header and footer content
+                    document.body.innerHTML = headerData + document.body.innerHTML + footerData;
+                })
+                .catch(error => console.error("Error fetching footer content:", error));
         })
-        .catch(error => console.error("Error fetching common content:", error));
+        .catch(error => console.error("Error fetching header content:", error));
 
     // Add your existing script functionality here
     // ...
 });
+
 
 
 // JavaScript to handle the FAQ dropdown functionality
