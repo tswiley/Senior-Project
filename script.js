9403d64e0eb0f5c4224542b8b67976cb8abf4c8a
Travis Wiley <traviswiley88@gmail.com>
2023-11-09 20:47:31 -0800

    Working on cart

--- script.js
+++ script.js
@@ -21,9 +21,58 @@ document.addEventListener("DOMContentLoaded", function () {
                     // Append the footer content to the body
                     document.body.appendChild(...footerContainer.childNodes);
 
+                    // Add event listener for the cart icon
+                    const cartIcon = document.getElementById('cart-icon');
+                    const cartContent = document.getElementById('cart-content');
+
+                    cartIcon.addEventListener('click', function () {
+                        console.log('Cart icon clicked');
+                        // Toggle the visibility of the cart content
+                        cartContent.classList.toggle('open');
+                        document.body.style.overflow = cartContent.classList.contains('open') ? 'hidden' : 'auto';
+
+                        // Call the updateCart function when the cart is opened
+                        if (cartContent.classList.contains('open')) {
+                            updateCart();
+                        }
+                    });
+
+                    // Function to update the cart items and total
+                    function updateCart() {
+                        // Fetch cart items from store.html
+                        const cartItemsContainer = document.getElementById('cart-items');
+                        const cartItems = Array.from(cartItemsContainer.children).map(item => {
+                            return {
+                                name: item.dataset.name,
+                                price: parseFloat(item.dataset.price),
+                            };
+                        });
+
+                        // Clear existing cart content
+                        cartItemsContainer.innerHTML = '';
+
+                        // Populate cart items or display "Your cart is empty"
+                        if (cartItems.length > 0) {
+                            cartItems.forEach(item => {
+                                const cartItemElement = document.createElement('div');
+                                cartItemElement.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+                                cartItemsContainer.appendChild(cartItemElement);
+                            });
+                        } else {
+                            const emptyCartMessage = document.createElement('div');
+                            emptyCartMessage.textContent = 'Your cart is empty.';
+                            cartItemsContainer.appendChild(emptyCartMessage);
+                        }
+
+                        // Calculate and display the total
+                        const cartTotal = cartItems.reduce((total, item) => total + item.price, 0);
+                        document.getElementById('cart-total').textContent = `Total: $${cartTotal.toFixed(2)}`;
+                    }
+
                     // Add event listener for the hamburger menu after content is loaded
                     const mobileMenu = document.getElementById('mobile-menu');
                     const nav = document.getElementById('nav');
+                    const closeIcon = document.getElementById('close-icon');
 
                     mobileMenu.addEventListener('click', function () {
                         console.log('Mobile menu clicked');
@@ -32,16 +81,23 @@ document.addEventListener("DOMContentLoaded", function () {
                         document.body.style.overflow = nav.classList.contains('show') ? 'hidden' : 'auto';
                     });
 
-                    // Add event listener for the cart icon
-                    const cartIcon = document.getElementById('cart-icon');
-                    const cart = document.getElementById('cart');
-
-                    cartIcon.addEventListener('click', function () {
-                        cart.classList.toggle('show-content');
+                    closeIcon.addEventListener('click', function () {
+                        console.log('Close icon clicked');
+                        mobileMenu.classList.remove('open');
+                        nav.classList.remove('show');
+                        document.body.style.overflow = 'auto';
                     });
 
-                    // Add your existing script functionality here
-                    // ...
+                    // Add event listener for closing the cart when clicking outside of it
+                    document.addEventListener('click', function (event) {
+                        const isCartIcon = event.target.id === 'cart-icon';
+                        const isCartContent = event.target.closest('#cart-content');
+
+                        if (!isCartIcon && !isCartContent && cartContent.classList.contains('open')) {
+                            cartContent.classList.remove('open');
+                            document.body.style.overflow = 'auto';
+                        }
+                    });
 
                 })
                 .catch(error => console.error("Error fetching footer content:", error));
