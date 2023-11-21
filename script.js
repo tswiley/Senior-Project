b750b66bcf85ef9c2c7aa8c5aab1140b8c18efff
Travis Wiley <traviswiley88@gmail.com>
2023-11-21 07:36:09 -0800

    padding to h1 and h2 changed index.html

--- script.js
+++ script.js
@@ -17,103 +17,26 @@ document.addEventListener("DOMContentLoaded", function () {
         return img;
     });
 
-    function fadeOut() {
-        const mainImg = mainImgContainer.querySelector('img');
-        mainImg.style.opacity = 0; // Set opacity to 0 for fade-out
-        mainImgContainer.classList.add('fade-out');
-    }
-
     function fadeIn() {
-        const mainImg = imageObjects[currentIndex].cloneNode(true);
-        mainImg.alt = 'Main Image';
+        const mainImg = mainImgContainer.querySelector('img');
+        mainImg.src = imageObjects[currentIndex].src;
         mainImg.style.opacity = 0; // Set initial opacity to 0
 
-        mainImgContainer.innerHTML = '';
-        mainImgContainer.appendChild(mainImg);
-
-        // Trigger reflow to apply the fade-in class
+        // Trigger reflow to apply the fade-in effect
         void mainImgContainer.offsetWidth;
 
         mainImg.style.opacity = 1; // Set opacity to 1 for fade-in
-        mainImgContainer.classList.remove('fade-out');
-
-        // Add controls to the DOM
-        const controls = document.createElement('div');
-        controls.classList.add('controls');
-
-        const prevBtn = document.createElement('button');
-        prevBtn.id = 'prevBtn';
-        prevBtn.type = 'button';
-        prevBtn.classList.add('arrow', 'prev-arrow');
-        prevBtn.innerHTML = '<i class="fas fa-chevron-left"></i>';
-        controls.appendChild(prevBtn);
-
-        const playPauseBtn = document.createElement('button');
-        playPauseBtn.type = 'button';
-        playPauseBtn.textContent = 'Pause';
-        playPauseBtn.id = 'playPauseBtn';
-        playPauseBtn.classList.add('play-pause');
-        controls.appendChild(playPauseBtn);
-
-        const nextBtn = document.createElement('button');
-        nextBtn.id = 'nextBtn';
-        nextBtn.type = 'button';
-        nextBtn.classList.add('arrow', 'next-arrow');
-        nextBtn.innerHTML = '<i class="fas fa-chevron-right"></i>';
-        controls.appendChild(nextBtn);
-
-        mainImgContainer.appendChild(controls);
-
-        // Add event listener for play/pause toggle
-        playPauseBtn.addEventListener('click', playPause);
     }
 
     function rotateImage() {
-        fadeOut();
+        const mainImg = mainImgContainer.querySelector('img');
+        mainImg.style.opacity = 0 // Set opacity to 0 for fade-out
         currentIndex = (currentIndex + 1) % imageList.length;
+
         setTimeout(() => {
             fadeIn(); // Trigger fade-in after fade-out
         }, 1000); // Adjust the duration as needed
     }
-
-    function playPause() {
-        const playPauseBtn = document.getElementById('playPauseBtn');
-        if (intervalId) {
-            clearInterval(intervalId);
-            intervalId = null;
-            playPauseBtn.innerText = 'Play';
-        } else {
-            intervalId = setInterval(rotateImage, 5000);
-            playPauseBtn.innerText = 'Pause';
-        }
-    }
-
-    function navigateImage(direction) {
-        if (direction === 'prev') {
-            currentIndex = (currentIndex - 1 + imageList.length) % imageList.length;
-        } else {
-            currentIndex = (currentIndex + 1) % imageList.length;
-        }
-        fadeOut();
-        setTimeout(() => {
-            fadeIn(); // Trigger fade-in after fade-out
-        }, 1000); // Adjust the duration as needed
-    }
-
-    document.getElementById('prevBtn').addEventListener('click', (event) => {
-        event.preventDefault();
-        navigateImage('prev');
-    });
-
-    document.getElementById('playPauseBtn').addEventListener('click', (event) => {
-        event.preventDefault();
-        playPause();
-    });
-
-    document.getElementById('nextBtn').addEventListener('click', (event) => {
-        event.preventDefault();
-        navigateImage('next');
-    });
 
     // Initial display of the first image
     fadeIn();
